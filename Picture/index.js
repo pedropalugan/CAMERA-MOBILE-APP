65a3a2d0ea3efea7a67034b1cb9e269da35fc697
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-08-04 14:33:53 -0300

    Update index.js

--- Picture/index.js
+++ Picture/index.js
@@ -1,35 +1,42 @@
 import React, { useState, useRef, useEffect } from 'react';
-import {View, TouchableOpacity, Text, SafeAreaView, Image, Modal, StyleSheet, Share} from 'react-native'
-import { Camera } from 'expo-camera'
-import { FontAwesome } from '@expo/vector-icons'
-import * as Permissions from 'expo-permissions'
-import * as MediaLibrary from 'expo-media-library'
-import * as Sharing from 'expo-sharing'; 
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  Image,
+  Modal,
+  StyleSheet,
+  Share,
+} from 'react-native';
+import { Camera } from 'expo-camera';
+import { FontAwesome } from '@expo/vector-icons';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+import * as Sharing from 'expo-sharing';
 
-
-export default function Picture(){
-  const camRef = useRef(null)
-  const [boltColor, setBoltcolor] = useState('#FFF')
-  const [backColor, setBackcolor] = useState('#000')
+export default function Picture() {
+  const camRef = useRef(null);
+  const [boltColor, setBoltcolor] = useState('#FFF');
+  const [backColor, setBackcolor] = useState('#000');
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHaspermission] = useState(null);
   const [capturedPhoto, setCapturedPhoto] = useState(null);
-  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off)
-  const [flashType, setFlashType] = useState('OFF')
-  const [open, setOpen] = useState(false)
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const [flashType, setFlashType] = useState('OFF');
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    if(flash === 1){
-      setFlashType("ON")
-      setBoltcolor("#000")
-      setBackcolor("#FFF")
+    if (flash === 1) {
+      setFlashType('ON');
+      setBoltcolor('#000');
+      setBackcolor('#FFF');
+    } else if (flash === 0) {
+      setFlashType('OFF');
+      setBoltcolor('#FFF');
+      setBackcolor('#000');
     }
-    else if (flash === 0){
-      setFlashType("OFF")
-      setBoltcolor("#FFF")
-      setBackcolor("#000")
-    }
-  }, [flash])
+  }, [flash]);
 
   useEffect(() => {
     (async () => {
@@ -40,132 +47,152 @@ export default function Picture(){
       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       setHaspermission(status === 'granted');
     })();
-  }, [])
+  }, []);
 
   if (hasPermission === null) {
-    return <View />
+    return <View />;
   }
 
   if (hasPermission === false) {
-    return <Text>Acesso negado!!!</Text>
+    return <Text>Acesso negado!!!</Text>;
   }
 
-  async function takePicture(){
-    if(camRef){
-      const data = await camRef.current.takePictureAsync()
-      setCapturedPhoto(data.uri)
-      setOpen(true)
+  async function takePicture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true);
     }
   }
 
-  async function savePicture(){
+  async function savePicture() {
     const asset = await MediaLibrary.createAssetAsync(capturedPhoto)
-    .then(() => {
-      alert("Foto salva com sucesso!!!")
-    })
-    .catch(error => console.log(error))
+      .then(() => {
+        alert('Foto salva com sucesso!!!');
+      })
+      .catch((error) => console.log(error));
   }
 
-    const onShare = async () => {
-    await Sharing.shareAsync(capturedPhoto) 
-  }
+  const onShare = async () => {
+    await Sharing.shareAsync(capturedPhoto);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Camera
-        style={{ flex: 1 }}
-        type={type}
-        ref={camRef}
-        flashMode={flash}
-      >
-        <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
-          <TouchableOpacity style={{
-            position: 'absolute',
-            bottom: 32,
-            left: 20
-          }}
-            onPress={() => setType(
-              type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-            )}>
-            <FontAwesome name='repeat' size={23} color="#FFF"/>
-
+      <Camera style={{ flex: 1 }} type={type} ref={camRef} flashMode={flash}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              bottom: 32,
+              left: 20,
+            }}
+            onPress={() =>
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              )
+            }>
+            <FontAwesome name="repeat" size={23} color="#FFF" />
           </TouchableOpacity>
 
-          <TouchableOpacity style={{
-            position: 'absolute',
-            bottom: 25,
-            left: 80,
-            borderRadius: '50%',
-            width: 40,
-            backgroundColor:backColor,
-            height: 40,
-            alignItems: 'center',}}
-            onPress={() => setFlash(
-              flash === Camera.Constants.FlashMode.off
-              ? Camera.Constants.FlashMode.on 
-              : Camera.Constants.FlashMode.off 
-            )}
-            >
-          <FontAwesome name='flash' size={23} color={boltColor} style={{marginTop: '20%'}}/>
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              bottom: 25,
+              left: 80,
+              borderRadius: '50%',
+              width: 40,
+              backgroundColor: backColor,
+              height: 40,
+              alignItems: 'center',
+            }}
+            onPress={() =>
+              setFlash(
+                flash === Camera.Constants.FlashMode.off
+                  ? Camera.Constants.FlashMode.on
+                  : Camera.Constants.FlashMode.off
+              )
+            }>
+            <FontAwesome
+              name="flash"
+              size={23}
+              color={boltColor}
+              style={{ marginTop: '20%' }}
+            />
           </TouchableOpacity>
         </View>
       </Camera>
-      <TouchableOpacity style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'gray',
-        margin: 20,
-        borderRadius: 10,
-        height: 50,
-      }} onPress={takePicture}>
+      <TouchableOpacity
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'gray',
+          margin: 20,
+          borderRadius: 10,
+          height: 50,
+        }}
+        onPress={takePicture}>
         <FontAwesome name="camera" size={23} color="#FFF" />
       </TouchableOpacity>
 
-      { capturedPhoto &&
-        <Modal 
-        animationType='slide'
-        trasparent={false}
-        visible={open}
-        >
-          <View style={{
-            flex:1, justifyContent:"center", alignItems: 'center'
-          }}>
+      {capturedPhoto && (
+        <Modal animationType="slide" trasparent={false} visible={open}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                marginTop: 100,
+                marginBottom: 40,
+                justifyContent: 'space-between',
+                alignContent: 'center',
+                width: '50%',
+              }}>
+              <TouchableOpacity onPress={savePicture} style={{}}>
+                <FontAwesome name="upload" size={50} color="#121212" />
+              </TouchableOpacity>
 
-          <View style={{flex: 1, flexDirection: 'row', marginTop: 100, marginBottom: 40, justifyContent: 'space-between', alignContent: 'center', width: '50%'}}>
-          <TouchableOpacity onPress={ savePicture } style={{}}>
-            <FontAwesome name='upload' size={50} color="#121212"/>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={ onShare }>
-            <FontAwesome name='share' size={50} color="#121212"/>
-          </TouchableOpacity>
+              <TouchableOpacity onPress={onShare}>
+                <FontAwesome name="share" size={50} color="#121212" />
+              </TouchableOpacity>
+            </View>
           </View>
+          <Image
+            style={{ width: '100%', height: 450, borderRadius: 10 }}
+            source={{ uri: capturedPhoto }}
+          />
 
-          </View>
-            <Image style={{width: '100%', height:450, borderRadius: 10}}
-            source={{uri: capturedPhoto}}
-            />
-
-        <View style={{
-            flex:1, justifyContent:"center", alignItems: 'center'
-          }}>
-          <TouchableOpacity onPress={() => setOpen(false)}>
-            <FontAwesome name='window-close' size={50} color="#FF0000"/>
-          </TouchableOpacity>
-
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity onPress={() => setOpen(false)}>
+              <FontAwesome name="window-close" size={50} color="#FF0000" />
+            </TouchableOpacity>
           </View>
         </Modal>
-      }
-
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
